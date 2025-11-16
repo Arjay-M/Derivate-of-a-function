ac12495fe6ab312f43acf72f3ab4ca55e2aa055c
Arjay-M <ACMacagba1@MY.SCCCD.EDU>
2025-11-16 13:59:31 -0800

    Update specialFunctionsForPoly143.m

--- specialFunctionsForPoly143.m
+++ specialFunctionsForPoly143.m
@@ -1,1 +1,10 @@
+%derivative and integral of a ploynomail 14.3
+p = input("Enter the coeffients of a cubic polynomail, as a matrix[...]\n>");
+y0 = input ("Enter the integration constant for that function.\n>");
+pDer = polyder(p);
+pint = polyint(p,y0);
+
+%report results
+fprintf ("The derivative of the function is %.2gx^2+%.2gx+%.2g \n",pDer)
+fprintf ("The integral of the function is %.2gx^4+%.2gx^3+%.2gx^2+%.2gx+%.2g \n",pint)
 
